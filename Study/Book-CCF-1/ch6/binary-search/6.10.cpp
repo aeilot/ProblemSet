63d197d060eeb4fed749c4b7ee02d23369eb0377
Louis Aeilot <1329415023@qq.com>
2021-01-17 17:02:57 +0800

    Update Binary Search

---
+++ Study/Book-CCF-1/ch6/binary-search/6.10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace    std;
+
+int Ns[10000];
+
+int main(int argc, char* argv[]){
+    int n,m;
+    cin >> n >> m;
+    for (int i=1; i<=n; i++) cin >> Ns[i];
+    Ns[0] = -1;
+    for (int i=0; i<m; i++){
+        int t;
+        cin >> t;
+        int lt=1, rt=n, mid;
+        while(lt <= rt){
+            mid=(lt+rt)/2;
+            if(Ns[mid]>t){
+                rt = mid - 1;
+            }else{
+                lt = mid + 1;
+            }
+        }
+        cout << Ns[rt] << endl;
+    }
+    return 0;
+}
+
