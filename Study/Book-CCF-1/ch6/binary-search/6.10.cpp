434775b16bd5be6eeeed9beadefce8e39347ec63
Louis Aeilot <1329415023@qq.com>
2021-04-10 12:37:32 +0800

    Initial Commit

--- Study/Book-CCF-1/ch6/binary-search/6.10.cpp
+++ Study/Book-CCF-1/ch6/binary-search/6.10.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 
-using namespace    std;
+using namespace std;
 
 int Ns[10000];
 
-int main(int argc, char* argv[]){
-    int n,m;
+int main(int argc, char *argv[]) {
+    int n, m;
     cin >> n >> m;
-    for (int i=1; i<=n; i++) cin >> Ns[i];
+    for (int i = 1; i <= n; i++) cin >> Ns[i];
     Ns[0] = -1;
-    for (int i=0; i<m; i++){
+    for (int i = 0; i < m; i++) {
         int t;
         cin >> t;
-        int lt=1, rt=n, mid;
-        while(lt <= rt){
-            mid=(lt+rt)/2;
-            if(Ns[mid]>t){
+        int lt = 1, rt = n, mid;
+        while (lt <= rt) {
+            mid = (lt + rt) / 2;
+            if (Ns[mid] > t) {
                 rt = mid - 1;
-            }else{
+            } else {
                 lt = mid + 1;
             }
         }
