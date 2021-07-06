311b55eef55d9bd40aa03416944b377cafcf725f
Louis Aeilot <1329415023@qq.com>
2021-07-06 14:16:11 +0800

    Add LeetCode509,42

---
+++ LeetCode/509/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace	std;
+
+int main(int argc, char* argv[]){
+	int a[40];
+	a[0] = 0;
+	a[1] = 1;
+	
+	cout << "{0,1";
+	for(int i = 2; i<40; i++){
+		a[i] = a[i-1] + a[i-2];
+		cout << "," << a[i];
+	}
+	cout << "}" << endl;
+	return 0;
+}
+
