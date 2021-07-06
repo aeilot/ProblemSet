697910786d60297f5b09025850b46b425b0f1618
Louis Aeilot <1329415023@qq.com>
2021-07-06 23:35:50 +0800

    Add CSP-J

---
+++ LuoGu/P7071/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace	std;
+
+int main(int argc, char* argv[]){
+	int n;
+	cin >> n;
+	if(n%2!=0) cout << -1 << endl;
+	else{
+		int ar[5000000];
+		for(int i = 0; i<5000000; i++) ar[i] = 0;
+		int count = 0;
+		while(n!=0){
+			n/=2;
+			int tmp = n%2;
+			ar[count++] = tmp;
+		}
+		for(int i = count-1; i>=0; i--){
+			if(ar[i]==0) continue;
+			double tmp2 = pow(2, i+1);
+			printf("%.0f ", tmp2);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
