751bb8778ba9ef70cf04fab98e6d080e0facbb01
Louis Aeilot <1329415023@qq.com>
2021-09-25 15:31:00 +0800

    Solve Knapsack DP *2

---
+++ LuoGu/P1616/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace	std;
+
+#define LL long long
+
+const int maxm = 10000, maxt = 10000000;
+LL v[maxm], w[maxm];
+LL dp[maxt];
+
+int main(int argc, char* argv[]){
+	int t,m;
+	cin >> t >> m;
+	for(int i = 0; i<m; i++) cin >> w[i] >> v[i];
+	for(int i = 0; i<m; i++)
+		for(int j = w[i]; j<=t; j++)
+			dp[j] = max(dp[j], dp[j-w[i]]+v[i]);
+	cout << dp[t] << endl;
+	return 0;
+}
+
