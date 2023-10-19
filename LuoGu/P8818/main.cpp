23c384104eaba336944ccbb982e1a94a1b56f9e6
Louis <1329415023@qq.com>
2023-10-19 18:13:15 +0800

    Update

---
+++ LuoGu/P8818/main.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+long long A_max[100010][25], A_min[100010][25], B_max[100010][25],
+    B_min[100010][25], A_min_pos[100010][25], A_max_neg[100010][25];
+int LLOG[100010];
+
+int main() {
+    int n, m, q;
+    cin >> n >> m >> q;
+    for (int i = 1; i <= n; i++) {
+        int a;
+        cin >> a;
+        A_max[i][0] = A_min[i][0] = a;
+        if (a > 0)
+            A_max_neg[i][0] = INT_MIN;
+        else
+            A_max_neg[i][0] = a;
+        if (a < 0)
+            A_min_pos[i][0] = INT_MAX;
+        else
+            A_min_pos[i][0] = a;
+    }
+    for (int i = 1; i <= m; i++) {
+        int a;
+        cin >> a;
+        B_max[i][0] = B_min[i][0] = a;
+    }
+    for (int i = 2; i <= max(m, n); i++) LLOG[i] = LLOG[i / 2] + 1;
+    for (int i = 1; i <= LLOG[n]; i++) {
+        for (int j = 1; j + (1 << i) - 1 <= n; j++) {
+            A_max[j][i] =
+                max(A_max[j][i - 1], A_max[j + (1 << (i - 1))][i - 1]);
+            A_min[j][i] =
+                min(A_min[j][i - 1], A_min[j + (1 << (i - 1))][i - 1]);
+            A_max_neg[j][i] =
+                max(A_max_neg[j][i - 1], A_max_neg[j + (1 << (i - 1))][i - 1]);
+            A_min_pos[j][i] =
+                min(A_min_pos[j][i - 1], A_min_pos[j + (1 << (i - 1))][i - 1]);
+        }
+    }
+    for (int i = 1; i <= LLOG[m]; i++) {
+        for (int j = 1; j + (1 << i) - 1 <= m; j++) {
+            B_max[j][i] =
+                max(B_max[j][i - 1], B_max[j + (1 << (i - 1))][i - 1]);
+            B_min[j][i] =
+                min(B_min[j][i - 1], B_min[j + (1 << (i - 1))][i - 1]);
+        }
+    }
+    // Q
+    while (q--) {
+        int l1, r1, l2, r2;
+        cin >> l1 >> r1 >> l2 >> r2;
+        int d1 = LLOG[r1 - l1 + 1];
+        int d2 = LLOG[r2 - l2 + 1];
+        long long amx = max(A_max[l1][d1], A_max[r1 - (1 << d1) + 1][d1]);
+        long long amn = min(A_min[l1][d1], A_min[r1 - (1 << d1) + 1][d1]);
+        long long bmx = max(B_max[l2][d2], B_max[r2 - (1 << d2) + 1][d2]);
+        long long bmn = min(B_min[l2][d2], B_min[r2 - (1 << d2) + 1][d2]);
+        long long amx_ng =
+            max(A_max_neg[l1][d1], A_max_neg[r1 - (1 << d1) + 1][d1]);
+        long long amn_ps =
+            min(A_min_pos[l1][d1], A_min_pos[r1 - (1 << d1) + 1][d1]);
+        long long ans = LLONG_MIN;
+        if (amx >= 0) {
+            ans = max(ans, amx * bmn);
+        }
+        if (amn <= 0) {
+            ans = max(ans, amn * bmx);
+        }
+        if (bmx >= 0 && amx_ng != INT_MIN) ans = max(ans, amx_ng * bmx);
+        if (bmn <= 0 && amn_ps != INT_MAX) ans = max(ans, amn_ps * bmn);
+        cout << ans << endl;
+    }
+    return 0;
+}
---
+++ LuoGu/P8862/40pts.cpp
@@ -0,0 +1,140 @@
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long A[100010];
+
+struct Node {
+    long long v;
+    long long tg;
+} NDS[400040];
+
+int n, m;
+
+void build(int l, int r, int pl, int pr, int p) {
+    NDS[p].tg = 0;
+    if (pl == pr) {
+        NDS[p].v = A[pl];
+        return;
+    }
+    int mid = pr + ((pl - pr) >> 1);
+    build(l, r, pl, mid, p * 2);
+    build(l, r, mid + 1, pr, p * 2 + 1);
+    NDS[p].v = min(NDS[p * 2].v, NDS[p * 2 + 1].v);
+}
+
+void add(int l, int r, int pl, int pr, long long k, int p) {
+    if (pl >= l && pr <= r) {
+        NDS[p].v += k;
+        NDS[p].tg += k;
+        return;
+    }
+    int mid = pr + ((pl - pr) >> 1);
+    if (NDS[p].tg != 0 && l != r) {
+        NDS[p * 2].tg += NDS[p].tg;
+        NDS[p * 2].v += NDS[p].tg;
+        NDS[p * 2 + 1].tg += NDS[p].tg;
+        NDS[p * 2 + 1].v += NDS[p].tg;
+        NDS[p].tg = 0;
+    }
+    if (l <= mid) {
+        add(l, r, pl, mid, k, p * 2);
+    }
+    if (r > mid) {
+        add(l, r, mid + 1, pr, k, p * 2 + 1);
+    }
+    NDS[p].v = min(NDS[p * 2].v, NDS[p * 2 + 1].v);
+}
+
+long long get(int l, int r, int pl, int pr, int p) {
+    if (pl >= l && pr <= r) {
+        return NDS[p].v;
+    }
+    int mid = pr + ((pl - pr) >> 1);
+    long long ans = LLONG_MAX;
+    if (NDS[p].tg != 0 && l != r) {
+        NDS[p * 2].tg += NDS[p].tg;
+        NDS[p * 2].v += NDS[p].tg;
+        NDS[p * 2 + 1].tg += NDS[p].tg;
+        NDS[p * 2 + 1].v += NDS[p].tg;
+        NDS[p].tg = 0;
+    }
+    if (l <= mid) {
+        ans = min(get(l, r, pl, mid, p * 2), ans);
+    }
+    if (r > mid) {
+        ans = min(get(l, r, mid + 1, pr, p * 2 + 1), ans);
+    }
+    return ans;
+}
+
+struct Step {
+    int type;
+    long long op1, op2, op3;
+} OPs[100010];
+
+void clear() {
+    for (auto& i : NDS) {
+        i.tg = 0;
+        i.v = 0;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    long long T;
+    cin >> T;
+    while (T--) {
+        n = 0;
+        m = 0;
+        clear();
+        memset(A, 0, sizeof(A));
+        vector<long long> ops;
+        cin >> n >> m;
+        for (int i = 1; i <= n; i++) {
+            long long b;
+            cin >> b;
+        }
+        for (int i = 1; i <= m; i++) {
+            long long a;
+            cin >> a;
+            if (a == 1) {
+                long long l, r, x;
+                cin >> l >> r >> x;
+                OPs[i].op1 = l;
+                OPs[i].op2 = r;
+                OPs[i].op3 = x;
+                OPs[i].type = 1;
+            } else {
+                int l, r;
+                cin >> l >> r;
+                OPs[i].op1 = l;
+                OPs[i].op2 = r;
+                OPs[i].op3 = 0;
+                OPs[i].type = 2;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            cin >> A[i];
+        }
+        build(1, n, 1, n, 1);
+        for (int i = m; i >= 1; i--) {
+            Step stp = OPs[i];
+            if (stp.type == 1) {
+                add(stp.op1, stp.op2, 1, n, -stp.op3, 1);
+            } else {
+                ops.push_back(get(stp.op1, stp.op2, 1, n, 1));
+            }
+        }
+        for (int i = ops.size() - 1; i >= 0; i--) {
+            cout << ops[i] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
